d00eb70ee673fe2a96ec0988323259f3e7ea5931
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2026-06-10 22:25:49 +0300

    Rebuild Windows library

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -1,41 +1,4 @@
 import CMinpack
-import Foundation
-class ExpFitProblem {
-    let t: [Double] = [1]
-    let y: [Double] = [1]
-}
-let residualCallback: @convention(c) (
-    UnsafeMutableRawPointer?,
-    CInt,
-    CInt,
-    UnsafePointer<Double>?,
-    UnsafeMutablePointer<Double>?,
-    CInt
-) -> CInt = { rawProblem, m, n, xPtr, fvecPtr, iflag in
 
-    guard
-        let rawProblem,
-        let xPtr,
-        let fvecPtr
-    else {
-        return -1
-    }
-
-    let problem = Unmanaged<ExpFitProblem>
-        .fromOpaque(rawProblem)
-        .takeUnretainedValue()
-
-    let a = xPtr[0]
-    let b = xPtr[1]
-
-    for i in 0..<Int(m) {
-        let model = a * Foundation.exp(-b * problem.t[i])
-        fvecPtr[i] = model - problem.y[i]
-    }
-
-    return 0
-}
-
-print("hello")
-lmdif1(nil, nil, 0, 0, nil, nil, 0, nil, nil, 0)
-//lmdif1(nil, nil, )
+let result = lmdif1(nil, nil, 0, 0, nil, nil, 0, nil, nil, 0)
+print(result)
